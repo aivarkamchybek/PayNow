efc31011c3c812329ba5b1df7789f4bbc1f02ba8
aivarkamchybek <127583391+aivarkamchybek@users.noreply.github.com>
2023-09-04 23:35:04 -0500

    Base.htlm and dashboard-back.html updates

--- userauths/views.py
+++ userauths/views.py
@@ -40,14 +40,16 @@ def LoginView(request):
             if user is not None: # if there is a user
                 login(request, user)
                 messages.success(request, "You are logged.")
-                return redirect("core:index")
+                return redirect("account:account")
             else:
                 messages.warning(request, "Username or password does not exist")
                 return redirect("userauths:sign-in")
         except:
             messages.warning(request, "User does not exist")
 
-    
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged In")
+        return redirect("account:account")
         
     return render(request, "userauths/sign-in.html")
 
