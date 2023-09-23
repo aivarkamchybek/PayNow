40a3e5d9ce667151463b5da59bc5869e9c1190b9
aivarkamchybek <127583391+aivarkamchybek@users.noreply.github.com>
2023-09-23 08:23:56 -0500

    payment request page added

--- core/payment_request.py
+++ core/payment_request.py
@@ -78,3 +78,30 @@ def AmountRequestConfirmation(request, account_number, transaction_id):
     }
 
     return render(request, "payment_request/amount-request-confirmation.html", context)
+
+def AmountRequestFinalProcess(request, account_number, transaction_id):
+    account = Account.objects.get(account_number=account_number)
+    transaction = Transaction.objects.get(transaction_id=transaction_id)
+
+    if request.method == "POST":
+        pin_number = request.POST.get("pin-number")
+        if pin_number == request.user.account.pin_number:
+            transaction.status = "completed"
+            transaction.save()
+
+            messages.success(request, "Your payment request have been sent successfully.")
+            return redirect("core:amount-request-completed", account.account_number,  transaction.transaction_id)
+    else:
+        messages.warning(request, "An Error Occured, try again later")
+        return redirect("account:dashboard")
+
+
+def RequestCompleted(request, account_number, transaction_id):
+    account = Account.objects.get(account_number=account_number)
+    transaction = Transaction.objects.get(transaction_id=transaction_id)
+    context = {
+        "account":account,
+        "transaction":transaction,
+    }
+    
+    return render(request, "payment_request/amount-request-completed.html", context)
--- core/urls.py
+++ core/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path("amount-request/<account_number>", payment_request.AmountRequest, name="amount-request"),
     path("amount-request-process/<account_number>", payment_request.AmountRequestProcess, name="amount-request-process"),
     path("amount-request-confirmation/<account_number>/<transaction_id>/", payment_request.AmountRequestConfirmation, name="amount-request-confirmation"),
+    path("amount-request-final-process/<account_number>/<transaction_id>/", payment_request.AmountRequestFinalProcess, name="amount-request-final-process"),
+    path("amount-request-completed/<account_number>/<transaction_id>/", payment_request.RequestCompleted, name="amount-request-completed"),
 
 ]
