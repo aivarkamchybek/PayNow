0e376a02f4ed51fd3380bc69bfdd9ea789cd5b4a
aivarkamchybek <127583391+aivarkamchybek@users.noreply.github.com>
2023-10-06 23:56:20 -0500

    static updates

--- payment_project/settings.py
+++ payment_project/settings.py
@@ -128,6 +128,7 @@ LOGOUT_REDIRECT_URL = "userauths:sign-in"
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
