287c13c39114a4c7d95aefaaf5484c8d0773a5e2
aivarkamchybek <127583391+aivarkamchybek@users.noreply.github.com>
2023-10-09 21:59:50 -0500

    updates

--- payment_project/settings.py
+++ payment_project/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r8d8ej7gv+vco$6m&w%&#wgu7l3a7k=fik)zphpw99tluu^wd('
-
+# SECRET_KEY = 'django-insecure-r8d8ej7gv+vco$6m&w%&#wgu7l3a7k=fik)zphpw99tluu^wd('
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
