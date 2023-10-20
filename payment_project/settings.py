74eabd80ea72f1373dc6b4d98edf45ea2e58d473
aivarkamchybek <127583391+aivarkamchybek@users.noreply.github.com>
2023-10-19 17:42:31 -0700

    Updates

--- payment_project/settings.py
+++ payment_project/settings.py
@@ -105,8 +105,7 @@ DATABASES = {
     }
 }
 
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
