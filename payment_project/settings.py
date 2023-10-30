74806cb47e0c132f5914263e0386e7ddee657cd5
aivarkamchybek <127583391+aivarkamchybek@users.noreply.github.com>
2023-10-30 14:46:05 -0500

    updates

--- payment_project/settings.py
+++ payment_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-r8d8ej7gv+vco$6m&w%&#wgu7l3a7k=fik)zphpw99tluu^wd('
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["paynow-project.up.railway.app"]
 ALLOWED_HOSTS = ["*"]
