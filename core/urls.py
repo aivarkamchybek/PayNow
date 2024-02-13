d432a4241c03d31220d6809ee3c512aeaf05b449
aivarkamchybek <127583391+aivarkamchybek@users.noreply.github.com>
2024-02-12 20:33:59 -0600

    updates

--- core/urls.py
+++ core/urls.py
@@ -41,4 +41,5 @@ urlpatterns = [
     path("fund_credit_card/<card_id>/", credit_card.fund_credit_card, name="fund-credit-card"),
     path("withdraw_fund/<card_id>/", credit_card.withdraw_fund, name="withdraw_fund"),
     path("delete_card/<card_id>/", credit_card.delete_card, name="delete_card"),
+    path('all-cards/', credit_card.all_cards, name='all-cards'),
 ]
